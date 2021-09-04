a25424727523598a094bfd88d1243e63ee058b35
kanchi0914 <kandent914@gmail.com>
2021-09-04 16:48:47 +0900

    update

--- Assets/Demo.cs
+++ Assets/Demo.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using EGUI.Base;
 using EGUI.Demo;
 using EGUI.GameObjects;
 using EGUI.GameObjects.Demos;
@@ -7,6 +9,11 @@ using UnityEngine;
 
 public class Demo : MonoBehaviour
 {
+    private void Update()
+    {
+        Debug.Log("bbbbbb");
+    }
+
     void Start()
     {
         // new ButtonDemo();
@@ -15,7 +22,26 @@ public class Demo : MonoBehaviour
         //     .AddOption("text2", null)
         //     .AddOption("text3", null)
         //     .SetSize(200,50);
+        
+        var aaa = new EGText().SetText("bbbbb").SetAnchorType(AnchorType.MiddleCenter);
 
-        new EgSlider().SetSize(200, 40);
+        // var par = new EGGameObject().SetSize(400, 100);
+        //
+        // new EGToggle(par).SetAnchorType(AnchorType.TopLeft).SetRelativeSize(.33f, 1f);
+        // new EGToggle(par).SetAnchorType(AnchorType.TopLeft).SetRelativePosition(.33f, 0).SetRelativeSize(.33f, 1f);
+        // new EGToggle(par).SetAnchorType(AnchorType.TopLeft).SetRelativePosition(.66f, 0).SetRelativeSize(.33f, 1f);
+        
+        // new EGToggle(isWithCheckBoxImage:false)
+        //     .SetImageColor(Color.white)
+        //     .SetSize(200, 50);
+        //
+        // new EGToggle(isWithCheckBoxImage:false)
+        //     .SetImageColor(Color.white)
+        //     .SetPosition(200, 0)
+        //     .SetSize(200, 50);
+
+        //new ButtonAndWindow();
+
+        // new EgSlider().SetSize(200, 40);
     }
 }
